9e96b1705f2f647f091b780ad54fb90235e102c3
unknown <christopherbonime@gmail.com>
2022-12-07 18:37:06 -0500

    added comments to js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,42 @@
 $( document ).ready(function() {
 
-  $(".saveBtn").click(function () {
-   var value = $(this).siblings(".description").val();
-   var time = $(this).parent().attr("id")
-   localStorage.setItem(time, value);
-  });
-  
-  $(".time-block").each(function () {
-    var currentHour = parseInt(dayjs().format("H"));
-    var plannerHour = parseInt($(this).attr("id"));
-    if (currentHour < plannerHour) {
-      $(this).addClass("future");
-     } else if (currentHour === plannerHour) {
-      $(this).removeClass("future");
-      $(this).addClass("present");
-    } else {
-      $(this).removeClass("present");
-      $(this).removeClass("future");
-      $(this).addClass("past");
-    }
-  });
+//ADDING CLICK EVENT TO SAVE BUTTON 
+$(".saveBtn").click(function () {
+  // DEFINING VARIABLES FOR TEXT INPUT FIELD AND TIME
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id")
+  // SENDING THESE VARIABLES TO LOCAL STORAGE
+  localStorage.setItem(time, value);
+});
 
-  $("#9 .description").val(localStorage.getItem("9"));
-  $("#10 .description").val(localStorage.getItem("10"));
-  $("#11 .description").val(localStorage.getItem("11"));
-  $("#12 .description").val(localStorage.getItem("12"));
-  $("#13 .description").val(localStorage.getItem("13"));
-  $("#14 .description").val(localStorage.getItem("14"));
-  $("#15 .description").val(localStorage.getItem("15"));
-  $("#16 .description").val(localStorage.getItem("16"));
-  $("#17 .description").val(localStorage.getItem("16"));
+$(".time-block").each(function () {
+  // FORMATTING LOCAL TIME TO ONLY DISPLAY THE HOUR
+  var currentHour = parseInt(dayjs().format("H"));
+    // GRABBING THE ID FOR EACH PLANNER SECTION TO COMPARE TO LOCAL TIME
+  var plannerHour = parseInt($(this).attr("id"));
+  // CHANGING THE BACKGROUND COLOR DEPENDING ON PAST, PRESENT OR FUTURE
+  if (currentHour < plannerHour) {
+    $(this).addClass("future");
+    } else if (currentHour === plannerHour) {
+    $(this).removeClass("future");
+    $(this).addClass("present");
+  } else {
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  }
+});
+
+  // GRABBING DATA FROM LOCAL STORAGE AND SENDING IT BACK TO THE PAGE
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("16"));
 
   // BUTTON TO CLEAR INPUT ITEMS AND LOCAL STORAGE
 $("#clearItemsBtn").click(function (event) {
@@ -38,6 +45,7 @@ $("#clearItemsBtn").click(function (event) {
   localStorage.clear();
 });
 
+  // USING DAYJS FOR LOCAL TIME, FORMATTING IT, AND THEN DISPLAYING IT IN THE HEADER
   var today = dayjs();
   $("#currentDay").text(today.format("dddd, MMMM D, YYYY h:mm A"))
 });
